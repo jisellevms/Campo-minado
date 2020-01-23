387fceca5572c92520c773683b136123df2e4466
jiselle <jiselle.santana@gfxconsultoria.com>
2020-01-23 15:16:54 -0300

    adicionandoo os primeiros blocos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Params from './params';
+import Field from './components/Field';
 
 export default class App extends Component{
     
     render(){
         return(
-            <View>
-                <Text>
-                    Jiselle
-                </Text>
+            <View style={style.container}>
+
+                <Field />
+                <Field opened />
+                <Field opened nearMines={1} />
+                <Field opened nearMines={2} />
+                <Field opened nearMines={3} />
+                <Field opened nearMines={6} />
             </View>
         )
     }
-
 }
+
+const style = StyleSheet.create({
+  container:{
+      flex: 1
+  }  
+})
